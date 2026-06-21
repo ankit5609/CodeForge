d87b78ced5f26668059c985ad6051237a8b3363e
ankit5609 <ankitkr5609@gmail.com>
2026-06-22 02:05:54 +0530

    reverse-proxy to handle preview url added

--- src/main/java/com/example/cybernode/ai/CodeForge/service/impl/KubernetesDeploymentServiceImpl.java
+++ src/main/java/com/example/cybernode/ai/CodeForge/service/impl/KubernetesDeploymentServiceImpl.java
@@ -8,6 +8,7 @@ import io.fabric8.kubernetes.client.dsl.ExecListener;
 import io.fabric8.kubernetes.client.dsl.ExecWatch;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
@@ -20,6 +21,7 @@ import java.util.concurrent.TimeUnit;
 public class KubernetesDeploymentServiceImpl implements DeploymentService {
 
     private final KubernetesClient client;
+    private final StringRedisTemplate redisTemplate;
 
     private static final String NAMESPACE = "cybernode-apps";
     private static final String POOL_LABEL = "status";
@@ -38,6 +40,7 @@ public class KubernetesDeploymentServiceImpl implements DeploymentService {
         Pod existingPod = findActivePod(projectId);
 
         if(existingPod != null) {
+            registerRoute(domain, existingPod);
             return new DeployResponse("http://"+domain+":"+REVERSE_PROXY_PORT);
         }
 
@@ -88,6 +91,13 @@ public class KubernetesDeploymentServiceImpl implements DeploymentService {
             client.pods().inNamespace(NAMESPACE).withName(podName).delete();
             throw new RuntimeException("Failed to deploy the project with id: "+projectId);
         }
+    }
+
+    private void registerRoute(String domain, Pod pod) {
+        String podIp = pod.getStatus().getPodIP();
+        if (podIp == null) throw new RuntimeException("Pod is running but has no IP!");
+
+        redisTemplate.opsForValue().set("route:" + domain, podIp + ":5173", 6, TimeUnit.HOURS);
     }
 
     private void execCommand(String podName, String container, String... command) {
